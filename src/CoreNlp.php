21c6b2b9a62acb7a1d57daa6585dc03eada15ae3
Olav <olav@web64.com>
2018-04-16 21:44:13 +0200

    added basic CoreNLP support

---
+++ src/CoreNlp.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Web64\Nlp;
+
+/**
+ *      wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-02-27.zip
+ *      unzip stanford-corenlp-full-2018-02-27.zip
+ *      cd stanford-corenlp-full-2018-02-27
+ *      java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
+ */
+class CoreNlp
+{
+    public $api_url = 'http://homestead:9000/';
+
+    public $properties = [];
+    public $data;
+
+    public function entities( $text )
+    {
+        $this->properties = [
+            'annotators'    => 'ner',
+            //'annotators'    => 'ner,sentiment',
+            //'annotators'    =>'tokenize,ssplit,pos,lemma,ner,depparse,coref,quote,sentiment',
+            'outputFormat'  => 'json',
+        ];
+        
+        $this->data = $this->post_call( $text );
+
+
+        $entities = [];
+        
+        if ( empty( $this->data['sentences']) )
+            return null;
+
+        
+        foreach(  $this->data['sentences'] as $sentence )
+        {
+            foreach( $sentence as $key => $value )
+            {
+                // echo " - {$key} \n";
+                // if ( $key == 'sentimentDistribution' || $key == 'sentiment' || $key == 'sentimentValue'   )
+                // {
+                    
+                //     print_r($value);
+                //     echo PHP_EOL;
+                // }
+
+                if ( $key == 'entitymentions' )
+                {
+                    foreach($value as $entity)
+                    {
+                        if ( !isset($entities[ $entity['ner'] ]) )
+                            $entities[ $entity['ner'] ] = [];
+                        
+                        if ( array_search( $entity['text'] , $entities[ $entity['ner'] ]) === false )
+                        {
+                            $entities[ $entity['ner'] ][] = $entity['text'];
+                        }
+
+                    }
+                    
+
+                }
+            }
+            
+        }
+        
+
+        return $entities;
+    }
+
+    public function post_call( $text )
+    {
+        $opts = array('http' =>
+        array(
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $text,
+        )
+    );
+    
+        $url = $this->api_url;
+        $url .= "?properties=" . urlencode( json_encode( $this->properties ) );
+        echo "URL: {$url} \n\n";
+
+        $context  = stream_context_create($opts);
+        $result = @file_get_contents($url, false, $context);
+
+        file_put_contents("corenlp.json", $result);
+        
+        return json_decode($result, 1);
+    }
+}
---
+++ tests/Unit/CoreNlpTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class CoreNlpTest extends TestCase 
+{
+    /** @test */
+    public function test_core_nlp()
+    {
+        $corenlp = new \Web64\Nlp\CoreNlp();
+        echo PHP_EOL. PHP_EOL;
+        $text = "Catalonia: Ex-police chief Trapero charged with sedition. The former chief of Catalonia's police force, Josep Lluis Trapero, has been charged over events linked with last year's independence referendum.";
+
+        $text = "German Foreign Ministry investigating arrest of German reporter in Turkey.
+        Germany's Foreign Ministry has said it is investigating claims that a German journalist has been detained in Turkey. Adil Demirci is believed to have been arrested while on vacation in Istanbul.
+        The German Foreign Ministry said on Friday that it was responding to reports that Adil Demirci, a journalist for the left-leaning Turkish news agency Etha, had been detained in Istanbul, although the reasons for his arrest remained unclear.
+        The detention of another journalist after the release of Deniz Yücel would be another obstacle in already frayed German-Turkish relations.
+        What we know about Adil Demirci's arrest so far
+
+    Reports suggested that Demirci was one of three Etha reporters detained during a series of police raids in Istanbul overnight on Thursday.
+    The German Foreign Ministry said it was \"working on the basis that Adil Demirci has been arrested,\" but admitted Turkish authorities had yet to confirm such reports.
+    Demirci reportedly worked as a Germany correspondent for Etha and lived in the city of Cologn.
+    News of Demirci's arrest was first reported by Mesale Tolu, a colleague of his at Etha. Tolu herself was one of several German nationals arrested in Turkey year last year for political reasons. Tolu tweeted on Friday morning that Demirci was detained with two colleagues, Pınar Gayip and Semiha Sahin, during a raid. Demirci, who reportedly holds both German and Turkish citizenship, was in Istanbul on holiday, Tolu said. Reports suggested he was scheduled to fly back to Germany on Saturday. 
+    ";
+        echo $text . PHP_EOL. PHP_EOL;
+        $entities = $corenlp->entities( $text );
+        print_r( $entities );
+
+
+        $this->assertNotEmpty( $entities['COUNTRY'] );
+    }
+}
